36cb00335c52eb9629060f176f0a4fcde1ef1322
Mahsa Shiri <mhsshr02@gmail.com>
2025-09-15 15:19:56 +0330

    Add a solution

---
+++ کمک به کاپی /komakBeCapy.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    int n;
+    string str;
+    cin >> n >> str;
+    for(int i = 0; i < n; i ++){
+        cout << "copy of" << " ";
+    }
+    cout << str << "\n";
+}
