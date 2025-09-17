f52b1fd2ee4aaf3a681964fee8ca43e05299333d
Mahsa Shiri <mhsshr02@gmail.com>
2025-09-17 21:18:06 +0330

    Add a solution

---
+++ شطرنج حرفه‌ای/shatranjHerfeiy.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int shah, vazir, rokh, fil, asb, sarbaz;
+    cin >> shah >> vazir >> rokh >> fil >> asb >> sarbaz;
+    cout << 1 - shah  << " " << 1 - vazir << " " << 2 - rokh << " " << 2 - fil << " " << 2 - asb << " " << 8 - sarbaz << "\n";
+}
