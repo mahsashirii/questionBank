309b4ab7b5b99d1485b181aac4892d555345b4cf
Mahsa Shiri <mhsshr02@gmail.com>
2025-09-19 00:05:08 +0330

    Add a solution

---
+++ تاریخ تولد/tarikhTavalod.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    string dateOfBirth;
+    cin >> dateOfBirth;
+    cout << "saal:" << dateOfBirth[0] << dateOfBirth[1] << "\n";
+    cout << "maah:" << dateOfBirth[2] << dateOfBirth[3] << "\n";
+}
